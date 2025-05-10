2cd41cf3fc4065eb8da3700d082be67d0e54a707
Елизавета <e.k.sorockoletova@yandex.ru>
2025-05-10 18:17:39 +0300

    Poll, several steps in term

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-import telebot
-from telebot import types # для указание типов
+import telebot # type: ignore
+from telebot import types # type: ignore # для указание типов
 import json
 
 with open("text.json", "r", encoding='utf-8') as f:
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(content['telegramToken'], parse_mode='MARKDOWN')
 userInfo = dict()
 
 
-def sendStage(message, currentStage):
+def sendStage(id, currentStage):
     global userInfo
     if not "buttons" in currentStage.keys() or len(currentStage['buttons'])==0:
         reply_markup = None
@@ -18,26 +18,26 @@ def sendStage(message, currentStage):
         for button in currentStage['buttons']:
             reply_markup.add(types.KeyboardButton(button["text"]))
     if "text" in currentStage.keys():
-        newText = currentStage["text"].replace("%%USERNAME%%", userInfo[message.chat.id]['name'])    
+        newText = currentStage["text"].replace("%%USERNAME%%", userInfo[id]['name'])    
         if not reply_markup is None:        
-            bot.send_message(message.chat.id, text=newText, reply_markup=reply_markup)
+            bot.send_message(id, text=newText, reply_markup=reply_markup)
         else:
-            bot.send_message(message.chat.id, text=newText)
+            bot.send_message(id, text=newText)
     if "photo" in currentStage.keys():
         for photo_path in currentStage["photo"]:
             with open(photo_path, 'rb') as photo_file:
-                bot.send_photo(message.chat.id, photo_file, reply_markup=reply_markup)
+                bot.send_photo(id, photo_file, reply_markup=reply_markup)
     if "audio" in currentStage.keys():
         for audio_path in currentStage["audio"]:
             with open(audio_path, 'rb') as audio_file:
-                bot.send_voice(message.chat.id, audio_file, reply_markup=reply_markup)
+                bot.send_voice(id, audio_file, reply_markup=reply_markup)
     if "circles" in currentStage.keys():
         for circle_path in currentStage["circles"]:
             with open(circle_path, 'rb') as circle_file:
-                bot.send_video_note(message.chat.id, circle_file, reply_markup=reply_markup)
+                bot.send_video_note(id, circle_file, reply_markup=reply_markup)
     if "poll" in currentStage.keys():
         pollInfo = currentStage['poll']
-        bot.send_poll(message.chat.id, pollInfo['question'], pollInfo['options'], type="quiz", correct_option_id=pollInfo['correct_option_id'])
+        bot.send_poll(id, pollInfo['question'], pollInfo['options'], type="quiz", correct_option_id=pollInfo['correct_option_id'], is_anonymous=False)
     
 
 @bot.message_handler(commands=['start']) 
@@ -47,8 +47,8 @@ def start_handler(message):
     # btn2 = types.KeyboardButton("Информация для родителей")
     # btn3 = types.KeyboardButton("Об экскурсии")
     # markup.add(btn1, btn2, btn3)
-    bot.send_message(message.chat.id, text="Привет, друг! Начать экскурсию /excursion \n Информация для родителей - /parents \n Об экскурсии - /about ".format(message.from_user))
-    userInfo[message.chat.id] = {
+    bot.send_message(message.from_user.id, text="Привет, друг! Начать экскурсию /excursion \n Информация для родителей - /parents \n Об экскурсии - /about ".format(message.from_user))
+    userInfo[message.from_user.id] = {
         'stage':-1,
         'answeredTerms':[], 
         "nameAnswered":False,
@@ -60,23 +60,23 @@ def start_handler(message):
 @bot.message_handler(commands=['excursion']) 
 def exc_handler(message):
     global userInfo
-    if userInfo[message.chat.id]['stage'] == -1: # блок до начала основного контента
-        userInfo[message.chat.id]['stage'] = 0         
-        sendStage(message, content['content'][userInfo[message.chat.id]['stage']])   
+    if userInfo[message.from_user.id]['stage'] == -1: # блок до начала основного контента
+        userInfo[message.from_user.id]['stage'] = 0         
+        sendStage(message.from_user.id, content['content'][userInfo[message.from_user.id]['stage']])   
     
 
 @bot.message_handler(commands=['parents']) 
 def parents_handler(message):
     global userInfo
-    if userInfo[message.chat.id]['stage'] == -1: # блок до начала основного контента
-        bot.send_message(message.chat.id, text='Информация для родителей')
+    if userInfo[message.from_user.id]['stage'] == -1: # блок до начала основного контента
+        bot.send_message(message.from_user.id, text='Информация для родителей')
 
 
 @bot.message_handler(commands=['about']) 
 def about_handler(message):
     global userInfo
-    if userInfo[message.chat.id]['stage'] == -1: # блок до начала основного контента
-        bot.send_message(message.chat.id, text='Об экскурсии')
+    if userInfo[message.from_user.id]['stage'] == -1: # блок до начала основного контента
+        bot.send_message(message.from_user.id, text='Об экскурсии')
 
 
 def check_for_term(message, currentStage):
@@ -103,42 +103,49 @@ def check_for_right_answer(message, currentStage):
 @bot.message_handler(content_types=['text'])
 def general_message_handler(message):
     global userInfo
-    if userInfo[message.chat.id]['stage'] == -1: # блок до начала основного контента
+    if userInfo[message.from_user.id]['stage'] == -1: # блок до начала основного контента
         if (message.text == 'Начать экскурсию'):
-            userInfo[message.chat.id]['stage'] = 0         
-            sendStage(message, content['content'][userInfo[message.chat.id]['stage']])                       
+            userInfo[message.from_user.id]['stage'] = 0         
+            sendStage(message.from_user.id, content['content'][userInfo[message.from_user.id]['stage']])                       
         elif (message.text == "Информация для родителей"): 
-            bot.send_message(message.chat.id, text='Информация для родителей')
+            bot.send_message(message.from_user.id, text='Информация для родителей')
         elif (message.text == "Об экскурсии"): 
-            bot.send_message(message.chat.id, text='Об экскурсии') 
+            bot.send_message(message.from_user.id, text='Об экскурсии') 
         return      
     else: # основной контент
-        currentStage = content['content'][userInfo[message.chat.id]['stage']]
+        currentStage = content['content'][userInfo[message.from_user.id]['stage']]
         if "actions" in currentStage.keys() and "term" in currentStage['actions']:
             res = check_for_term(message, currentStage) 
             if res is True:
-                userInfo[message.chat.id]['stage'] += 1
-                sendStage(message, content['content'][userInfo[message.chat.id]['stage']])
+                userInfo[message.from_user.id]['stage'] += 1
+                sendStage(message.from_user.id, content['content'][userInfo[message.from_user.id]['stage']])
             elif res is False:
-                userInfo[message.chat.id]['stage'] += 2
-                sendStage(message, content['content'][userInfo[message.chat.id]['stage']])
+                skip_steps = 1+1
+                if "term_skip_steps" in currentStage.keys():
+                    skip_steps = int(currentStage["term_skip_steps"])+1
+                userInfo[message.from_user.id]['stage'] += skip_steps
+                sendStage(message.from_user.id, content['content'][userInfo[message.from_user.id]['stage']])
             else:
-                bot.send_message(message.chat.id, text='Не понял ответ 🤔. Пожалуйста, нажми на одну из кнопок') 
+                bot.send_message(message.from_user.id, text='Не понял ответ 🤔. Пожалуйста, нажми на одну из кнопок') 
             return
         # проверка на ввод имени
         if "actions" in currentStage.keys() and "checkName" in currentStage['actions']:
-            userInfo[message.chat.id]['name'] = message.text
-            userInfo[message.chat.id]['nameAnswered'] = True
+            userInfo[message.from_user.id]['name'] = message.text
+            userInfo[message.from_user.id]['nameAnswered'] = True
         # check for right answer 
         if check_for_right_answer(message, currentStage): # ответ согласно кнопкам
-            userInfo[message.chat.id]['stage'] += 1
-            sendStage(message, content['content'][userInfo[message.chat.id]['stage']])
+            userInfo[message.from_user.id]['stage'] += 1
+            sendStage(message.from_user.id, content['content'][userInfo[message.from_user.id]['stage']])
         else:
-            bot.send_message(message.chat.id, text='Не понял ответ 🤔. Пожалуйста, нажми на одну из кнопок') 
+            bot.send_message(message.from_user.id, text='Не понял ответ 🤔. Пожалуйста, нажми на одну из кнопок') 
 
 
 @bot.poll_answer_handler()
 def handle_poll_answer(pollAnswer):
-    print(pollAnswer)
+    userInfo[pollAnswer.user.id]['stage'] += 1
+    sendStage(pollAnswer.user.id, content['content'][userInfo[pollAnswer.user.id]['stage']])
+
+    
+
 
 bot.polling(none_stop=True)
